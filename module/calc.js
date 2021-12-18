56a3b95ca4320227d09ee7d0e61cc7613f2d6d88
Tarun Chabarwal <tarunchabarwal@l4o.in>
2021-12-18 21:31:38 +0530

    Add common JS module

---
+++ module/calc.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const math = require('./math.js')
+
+let op = '+';
+let x = 10, y = 20;
+
+if (op == '+') {
+    console.log("sum is: " + math.add(x, y))
+}
+
+console.log("pi: " + math.PI)
---
+++ module/math.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const PI = 3.14
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+module.exports.PI = PI;
+module.exports.add = add;
+module.exports.subtract = subtract;
+module.exports.multiply = multiply;
+module.exports.divide = divide;
