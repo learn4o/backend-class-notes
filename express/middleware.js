1f9d38e90ce94d6ac830b71debc1b80f9bbbfe8a
Tarun Chabarwal <tarunchabarwal@l4o.in>
2022-01-23 17:19:32 +0530

    Add middleware

---
+++ express/middleware.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+
+function logRequest(req, res, next) {
+    console.log(`Request method: ${req.method}, url: ${req.url} at time: ${new Date()}`)
+    next()
+}
+
+app.use(logRequest)
+
+app.get('/', function (req, res) {
+  res.send('GET method called on /')
+})
+
+app.post('/', function (req, res) {
+    res.send('post method is called at /')
+})
+
+app.put('/', function (req, res) {
+    res.send("PUT method called at /")
+})
+
+app.get('/videos', function (req, res) {
+    console.log('path params', req.params)
+    console.log('query: ', req.query)
+    res.send('videos route called')
+})
+
+
+app.get('/confidential', function (req, res, next) {
+    console.log("Some confidential work is going on")
+    next()
+}, function (req, res) {
+    res.send("THIS is the ******* response")
+})
+
+app.use(function (req, res) {
+    res.status(404)
+    res.send('no routes defined')
+})
+
+app.listen(8080)
